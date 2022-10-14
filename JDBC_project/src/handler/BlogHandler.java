8a6a0cc3d63f37300371d452a7a5c615cc4a8586
AFejes22 <afejes22@gmail.com>
2022-10-14 20:48:02 +0200

    readPostData method created

--- JDBC_project/src/handler/BlogHandler.java
+++ JDBC_project/src/handler/BlogHandler.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.ZonedDateTime;
+import java.util.Scanner;
 
 public class BlogHandler {
 
@@ -26,32 +28,35 @@ public class BlogHandler {
     }
 
     public static void select (Connection c, String text) {
-        try(Statement st = c.createStatement()){
+        try(Statement st = c.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)){
             String query = "select * \r\n"
                     + "from blogs.phonebook\r\n"
-                    + "where number like" + "'%" + text + "%'" + "or name like" + "'%" + text + "%'";//a végére teljes elérési útvonal kell!
-            //String query = "SELECT id, name, number, created, slug FROM blogs.phonebook WHERE id = 1";//a végére teljes elérési útvonal kell!
+                    + "where number like" + "'%" + text + "%'" + "or name like" + "'%" + text + "%'";
+            //String query = "SELECT id, name, number, created, slug FROM blogs.phonebook WHERE id = 1";//
+            //String query = "select * from blogs.phonebook where "number" like '%jakab%' or "name" like '%jakab%';";//működik
             ResultSet rs = st.executeQuery(query);
 
 
-            if(!rs.next()){
-                System.out.println("No such name or number in the list!");
-            }else{
+            if(rs.next()){
+
                 System.out.println("The name or number you are looking for is based on the entry found:");
-            }
+                rs.beforeFirst();
 
-            while(rs.next()){
-                Post p = new Post(); //ezeket pl be lehet pakolni egy listába és akkot list-et adunk vissza, és azon lehet dolgozni
-                //pl stream()-okkal (filter, stb)
-                p.setId(rs.getLong("id"));
-                p.setName(rs.getString("name"));
-                p.setNumber(rs.getString("number"));
-                p.setSlug(rs.getString("slug"));
-                //p.setCreated(ZonedDateTime.ofInstant(rs.getTimestamp("created").toInstant(), ZoneId.systemDefault()));
-                //ehhez a Post class toString-ben a "skipp nulls" be van most állítva, mert szar a formátuma
+                while (rs.next()) {
+                    Post p = new Post(); //ezeket pl be lehet pakolni egy listába és akkot list-et adunk vissza, és azon lehet dolgozni
+                    //pl stream()-okkal (filter, stb)
+                    p.setId(rs.getLong("id"));
+                    p.setName(rs.getString("name"));
+                    p.setNumber(rs.getString("number"));
+                    p.setSlug(rs.getString("slug"));
+                    //p.setCreated(ZonedDateTime.ofInstant(rs.getTimestamp("created").toInstant(), ZoneId.systemDefault()));
+                    //ehhez a Post class toString-ben a "skipp nulls" be van most állítva, mert szar a formátuma
 
-                System.out.println(p);
+                    System.out.println(p);
 
+                }
+            } else {
+                System.out.println("No such name or number in the list!");
             }
 
         }catch(SQLException e) {
@@ -88,5 +93,26 @@ public class BlogHandler {
             System.err.println("State: " + e.getSQLState());
             e.printStackTrace();
         }
+    }
+
+    private static Post readPostData(Scanner scanner) {
+        try(scanner){
+            System.out.println("id: ");
+            Long id = scanner.nextLong();
+
+            System.out.println("name: ");
+            String name = scanner.nextLine();
+
+            System.out.println("number: ");
+            String number = scanner.nextLine();
+
+            System.out.println("slug: ");
+            String slug = scanner.nextLine();
+
+            Post post = new Post(id, name, number, slug, ZonedDateTime.now());
+
+            return post;
+        }
+
     }
 }
