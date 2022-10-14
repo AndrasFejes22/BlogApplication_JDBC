c7dd081be0e7d4f3a4e5c9cd671c00fa9f49d89e
AFejes22 <afejes22@gmail.com>
2022-10-14 13:48:27 +0200

    creata manu method in Main class

--- JDBC_project/src/main/Main.java
+++ JDBC_project/src/main/Main.java
@@ -6,20 +6,48 @@ import handler.BlogHandler;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
         System.out.println("Hello JDBC");
         ConnectionFactory connectionFactory = createConnectionFactory();
         //ITT vannak a menűpontok, ezt kell menűsíteni
-        try(Connection connection = connectionFactory.getConnection()){
-            new Main().run(connection);
+        try(Scanner scanner = new Scanner(System.in)){
+            new Main().run(scanner);
         }
     }
 
-    private void run(Connection connection) {
-        BlogHandler.queryAll(connection);
+    private void run(Scanner scanner) throws SQLException, IOException {
+        int menuItem;
+        try (scanner) {
+            ConnectionFactory connectionFactory = createConnectionFactory();
+            do {
+                printingMenu();
+                System.out.println();
+                menuItem = readInt("Please give a whole number!", scanner);
+                switch (menuItem) {
+                    case 1:
+                        try(Connection connection = connectionFactory.getConnection()){
+
+                            BlogHandler.queryAll(connection);
+
+                        }catch(SQLException e) {
+                            System.err.println("Error code: " + e.getErrorCode());
+                            System.err.println("Message: " + e.getMessage());
+                            System.err.println("State: " + e.getSQLState());
+                            e.printStackTrace();
+                        }
+                        System.out.println();
+                        break;
+
+                }
+
+            } while (menuItem != 0);
+        }
+
     }
 
     private static ConnectionFactory createConnectionFactory() throws IOException {
@@ -28,5 +56,40 @@ public class Main {
 
         ConnectionFactory connectionFactory = new ConnectionFactory(dbProperties);
         return connectionFactory;
+    }
+
+    static int readInt(String askMessage, Scanner scanner) {
+        boolean inputCorrect;
+        int number = 0;
+        do {
+            inputCorrect = true;
+            System.out.println(askMessage);
+            try {
+                number = scanner.nextInt();
+                if(number > 6 || number < 0){
+                    System.out.println("You can only enter a number between 0 and 5!");
+                    inputCorrect = false;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("This is not a valid integer!");
+                inputCorrect = false;
+            } finally {
+                scanner.nextLine();
+            }
+        } while (!inputCorrect);
+        return number;
+    }
+
+    public static void printingMenu() {
+        System.out.println("Please choose from the following menu items:");
+        System.out.println("--------------------------------------------");
+        System.out.println("1. queryAll");
+        System.out.println("2. "); //for tests, can put walls on top of each other and next to each other--> TO DO
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("5. ");
+        System.out.println( "\u001b[1;32m" + "NEW!" + "\u001b[0m");
+        System.out.println("0. Exit");
     }
 }
